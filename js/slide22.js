87479a4a47340df32c7df60527fc199bc115f5b3
StephSteve <stephanie.oncallbusiness@gmail.com>
2020-06-30 12:51:59 -0400

    edits for Day 2 Slide 22 javaScript

---
+++ js/slide22.js
@@ -0,0 +1,8 @@
+function getNumberSquared() {
+    var userInput = null;
+       while(userinput > 0 && userInput < 100) {
+            userInput = window.prompt("Please enter a number");
+            let numberSquared = userInput * userInput;
+        }
+return numberSquared;
+}
